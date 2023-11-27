8d83e69c9522f8a5bafc3dc14ec8b17a67e14462
asynkov <asynkov@belkacar.ru>
2023-11-27 09:08:52 +0300

    management service added

--- src/main/kotlin/com/synkov/management/notification/NotificationScheduler.kt
+++ src/main/kotlin/com/synkov/management/notification/NotificationScheduler.kt
@@ -80,8 +80,10 @@ class NotificationScheduler(
     private fun createNotifyAt(eventAt: LocalDateTime, zone: String): List<LocalDateTime> {
         val now = LocalDateTime.now(ZoneId.of("UTC")).offsetToZone(zone)
         return when {
-            eventAt.minusDays(3) > now -> listOf(
+            // more than 3 days to event
+            eventAt.minusDays(3) >= now -> listOf(
                 eventAt.minusMinutes(30),
+                eventAt.minusHours(4),
                 LocalDateTime.of(
                     eventAt.minusDays(1).toLocalDate(),
                     LocalTime.of(19, 0)
@@ -92,7 +94,8 @@ class NotificationScheduler(
                 )
             )
 
-            eventAt.minusDays(3) < now && now < eventAt.minusDays(1) -> listOf(
+            // from 3 to 2 days to event
+            eventAt.minusDays(3) <= now && now <= eventAt.minusDays(2) -> listOf(
                 eventAt.minusMinutes(30),
                 eventAt.minusHours(4),
                 LocalDateTime.of(
@@ -101,9 +104,50 @@ class NotificationScheduler(
                 )
             )
 
-            eventAt.minusDays(1) < now && now < eventAt.minusHours(1) -> listOf(
+            // from 48 to 40 hours to event
+            eventAt.minusHours(48) <= now && now <= eventAt.minusHours(40) -> listOf(
                 eventAt.minusMinutes(30),
                 eventAt.minusHours(4),
+                eventAt.minusHours(39)
+            )
+
+            // from 40 to 32 hours to event
+            eventAt.minusHours(40) <= now && now <= eventAt.minusHours(32) -> listOf(
+                eventAt.minusMinutes(30),
+                eventAt.minusHours(4),
+                eventAt.minusHours(31)
+            )
+
+            // from 32 to 24 hours to event
+            eventAt.minusHours(32) <= now && now <= eventAt.minusHours(24) -> listOf(
+                eventAt.minusMinutes(30),
+                eventAt.minusHours(4),
+                eventAt.minusHours(23)
+            )
+
+            // from 24 to 18 hours to event
+            eventAt.minusHours(24) <= now && now <= eventAt.minusHours(18) -> listOf(
+                eventAt.minusMinutes(30),
+                eventAt.minusHours(4),
+                eventAt.minusHours(23)
+            )
+
+            // from 18 to 10 hours to event
+            eventAt.minusHours(18) <= now && now <= eventAt.minusHours(10) -> listOf(
+                eventAt.minusMinutes(30),
+                eventAt.minusHours(4),
+                eventAt.minusHours(9)
+            )
+
+            // from 10 to 2 hours to event
+            eventAt.minusHours(10) <= now && now <= eventAt.minusHours(2) -> listOf(
+                eventAt.minusMinutes(30),
+                eventAt.minusHours(2)
+            )
+
+            // less than 2 hours to event
+            eventAt.minusHours(2) <= now -> listOf(
+                eventAt.minusMinutes(30)
             )
 
             else -> listOf()
